9c6265099f7222fbdeee2c95eee2bb1e37daf8ad
OlegPinigin <gtp1133@yandex.ru>
2023-08-03 22:24:55 +0500

    первый тест

---
+++ src/main/java/com/example/forSber/ForSberApplication.java
@@ -0,0 +1,13 @@
+package com.example.forSber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForSberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForSberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/forSber/ForSberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forSber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForSberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
